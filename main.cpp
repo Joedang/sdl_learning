4752b2f45c27e2c8c2e058802662af6e89bfe7dc
Joedang <shields6@pdx.edu>
2020-07-15 05:05:25 -0700

    twist on reading keypresses
    I diverged from the tutorial a bit. I used the keypresses to move an
    image around the window, rather than just display different images.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <iostream> // handle input/output streams (useful for piping text)
+#include <stdio.h> // nice functions for formatting/printing strings
+#include <SDL2/SDL.h> // API for handling multi-media user interaction
+#include <SDL2/SDL_image.h> // extension to SDL for loading non-BMP images
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -13,6 +14,14 @@ int main(int argc, char * argv[]) {
     SDL_Window * window = NULL;// a window to render things to
     SDL_Surface * windowSurface = NULL;// a surface to be contained by the window
     SDL_Surface * chris = NULL;// test image
+    char chrisFile[] = "img/chris.bmp";// test file to use
+    SDL_Rect chrisLocation;// where to draw the test image
+        chrisLocation.x = 50;
+        chrisLocation.y = 50;
+    SDL_Rect dstrect_dummy = chrisLocation;// for non-destructive location drawing
+    int chrisSpeed = 10;// how much to move chris per itteration
+    SDL_Event event;// event handler
+    bool quit = false;// whether to quit the program
 
     // initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -27,30 +36,64 @@ int main(int argc, char * argv[]) {
     if (window == NULL)
         sdlErr("Window could not be created.");
 
-    if (IMG_Init(IMG_INIT_JPG) < 0)// load the libraries for loading images
+    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) < 0)// load the libraries for loading images
         sdlErr("Failed to initialize image support.");
 
     windowSurface = SDL_GetWindowSurface(window);// get the reference to the window's surface (framebuffer)
 
-    chris = IMG_Load("img/chris.jpg");
+    chris = IMG_Load(chrisFile);
     if (chris == NULL)
         imgErr("Failed to load chris.");
-    //doge = SDL_LoadBMP("img/doge.bmp");
+    //doge = SDL_LoadBMP(chrisFile);
     //if (doge == NULL)
-    //    sdlErr("Failed to load doge.");
+    //    sdlErr("Failed to load chris.");
 
-    // draw stuff
-    SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 0xFF, 0xFF, 0xFF));// make that surface white
-    SDL_BlitSurface(chris, NULL, windowSurface, NULL);// draw the chris to the screen
-    SDL_UpdateWindowSurface(window);// update the surface
 
-    SDL_Delay(3000);// wait 3 seconds
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.type){
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym){
+                        case SDLK_UP:
+                            chrisLocation.y -= chrisSpeed;
+                            break;
+                        case SDLK_DOWN:
+                            chrisLocation.y += chrisSpeed;
+                            break;
+                        case SDLK_LEFT:
+                            chrisLocation.x -= chrisSpeed;
+                            break;
+                        case SDLK_RIGHT:
+                            chrisLocation.x += chrisSpeed;
+                            break;
+                    }
+                    break;
+                default:
+                    //std::cout << "SDL event type: " << std::to_string(event.type) << std::endl;
+                    break;
+            }
+        }
+        // TODO: I would like to know why keypresses seem to send events as if the program were a text editor, 
+        //       rather than just recognizing down/up events.
+        //       Pressing and holding an arrow key, the image moves one step, waits, and then moves smoothly.
+        // draw stuff
+        SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 0xAA, 0xAA, 0xAA));// fill that surface with a color
+        dstrect_dummy = chrisLocation;
+        SDL_BlitSurface(chris, NULL, windowSurface, &dstrect_dummy);// draw the chris to the screen
+        SDL_UpdateWindowSurface(window);// update the surface
+
+        SDL_Delay(100);// wait some ms
+    }
 
 	// free resources
 	SDL_DestroyWindow(window);// This also destroys the framebuffer referred to by windowSurface.
     SDL_FreeSurface(chris);
 
 	//Quit SDL subsystems
+    std::cout << "Goodbye." << std::endl;
     IMG_Quit();
 	SDL_Quit();
     return 0;
--- notes.md
+++ notes.md
@@ -2,7 +2,7 @@
 Remember that SDL is included with `#include <SDL2/SDL.h>`, 
 so the Lazy Foo tutorials will need to be tweaked in that way.
 
-### SDL2
+### SDL2_image
 The SDL2_Image documentation is very obnoxious, in that forgets that SDL2 is a thing.
 To use SDL2_image, it's required to have `#include <SDL2/SDL_image.h>`.
 However it's also necessary to link against it with the `-lSDL2_image` compiler flag.
@@ -18,3 +18,37 @@ The [SDL wiki](https://wiki.libsdl.org/) is also helpful.
 
 ### Tutorials
 I'm starting by following [the Lazy Foo tutorial](https://lazyfoo.net/tutorials/SDL/index.php#Hello%20SDL).
+
+## Events
+Use an object of type `SDL_Event` to buffer events.
+Use `SDL_PollEvent(SDL_Event * event)` to grab the events from the user.
+
+TODO: I would like to know why keypresses seem to send events as if the program were a text editor, 
+      rather than just recognizing down/up events.
+      Pressing and holding an arrow key, the image moves one step, waits, and then moves smoothly.
+
+### Exiting a Program
+The SDL_QUIT event
+SDL_QUIT is neccessary and sufficient for:
+- exiting a window with "Xing-out" or Alt+F4 via the WM
+- SIGINT from the terminal
+- maybe more...
+
+## Drawing to a Location
+`SDL_BlitSurface` is used to draw one surface onto another.
+(*Blit* is an old-school term for drawing an image.)
+Annoyingly, the destination rectangle is only used to convey location, 
+and that location gets mutated by `SDL_BlitSurface`.
+So, one way to avoid an important rectangle being overwritten is to use an intermediary rectangle.
+For example:
+```c++
+SDL_Rect chrisLocation;// where to draw the test image
+    chrisLocation.x = 50;
+    chrisLocation.y = 50;
+SDL_Rect dstrect_dummy = chrisLocation;// for non-destructive location drawing
+/* ... some code ... */
+dstrect_dummy = chrisLocation;
+SDL_BlitSurface(chris, NULL, windowSurface, &dstrect_dummy);// draw the chris to the screen
+```
+This would prevent `chrisLocation` from being overwritten, 
+which is what would happen if we simply passed `&chrisLocation` into `SDL_BlitSurface`.
