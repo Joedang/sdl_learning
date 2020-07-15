6428f2568d06a0e93df7eaab065892e62100e80f
Joedang <shields6@pdx.edu>
2020-07-15 02:10:35 -0700

    cleaned up defensive programming and .gitignore

--- main.cpp
+++ main.cpp
@@ -5,57 +5,55 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-void sdlErr(std::string);
-void imgErr(std::string);
+void sdlErr(std::string);// report an error related to SDL
+void imgErr(std::string);// report an error related to SDL_image
 
 int main(int argc, char * argv[]) {
-    // a window to render things to
-    SDL_Window * window = NULL;
 
-    // a surface to be contained by the window
-    SDL_Surface * windowSurface = NULL;
-
-    // test image
-    SDL_Surface * doge = NULL;
+    SDL_Window * window = NULL;// a window to render things to
+    SDL_Surface * windowSurface = NULL;// a surface to be contained by the window
+    SDL_Surface * chris = NULL;// test image
 
     // initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
         sdlErr("Failed to initialize SDL.");
-    } else {
-        window = SDL_CreateWindow( "SDL Learning", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-            sdlErr("Window could not be created.");
-        else {
-            if (IMG_Init(IMG_INIT_JPG) < 0)
-                sdlErr("Failed to initialize image support.");
-            // get the window surface
-            windowSurface = SDL_GetWindowSurface(window);
-            // get the image to display
-            doge = IMG_Load("img/doge.jpg");
-            if (doge == NULL)
-                imgErr("Failed to load doge.");
-            //doge = SDL_LoadBMP("img/doge.bmp");
-            //if (doge == NULL)
-            //    sdlErr("Failed to load doge.");
-            // make that surface white
-            SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 0xFF, 0xFF, 0xFF));
-            // draw the doge to the screen
-            SDL_BlitSurface(doge, NULL, windowSurface, NULL);
-            // update the surface
-            SDL_UpdateWindowSurface(window);
-            // wait 3 seconds
-            SDL_Delay(3000);
-        }
-    }
+
+    // create a window
+    window = SDL_CreateWindow( "SDL Learning", 
+                                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+                                SCREEN_WIDTH, SCREEN_HEIGHT, 
+                                SDL_WINDOW_SHOWN
+                             );
+    if (window == NULL)
+        sdlErr("Window could not be created.");
+
+    if (IMG_Init(IMG_INIT_JPG) < 0)// load the libraries for loading images
+        sdlErr("Failed to initialize image support.");
+
+    windowSurface = SDL_GetWindowSurface(window);// get the reference to the window's surface (framebuffer)
+
+    chris = IMG_Load("img/chris.jpg");
+    if (chris == NULL)
+        imgErr("Failed to load chris.");
+    //doge = SDL_LoadBMP("img/doge.bmp");
+    //if (doge == NULL)
+    //    sdlErr("Failed to load doge.");
+
+    // draw stuff
+    SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 0xFF, 0xFF, 0xFF));// make that surface white
+    SDL_BlitSurface(chris, NULL, windowSurface, NULL);// draw the chris to the screen
+    SDL_UpdateWindowSurface(window);// update the surface
+
+    SDL_Delay(3000);// wait 3 seconds
 
 	// free resources
-	SDL_DestroyWindow( window );
-    SDL_FreeSurface(doge);
+	SDL_DestroyWindow(window);// This also destroys the framebuffer referred to by windowSurface.
+    SDL_FreeSurface(chris);
 
 	//Quit SDL subsystems
     IMG_Quit();
 	SDL_Quit();
-    return(0);
+    return 0;
 }
 
 void sdlErr(std::string message){
