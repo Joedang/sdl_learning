ceada0c85579e73e0c5b62b4b98b92f9f969603b
Joedang <shields6@pdx.edu>
2020-07-23 15:33:08 -0700

    rendering text and using the mouse

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <stdio.h> // nice functions for formatting/printing strings
 #include <SDL2/SDL.h> // API for handling multi-media user interaction
 #include <SDL2/SDL_image.h> // extension to SDL for loading non-BMP images
+#include <SDL2/SDL_ttf.h> // extension to SDL for drawing text using TrueType fonts
 // }}}
 // globals {{{
-const int SCREEN_WIDTH = 1080; //640;
-const int SCREEN_HEIGHT = 720; //480;
+//const int SCREEN_WIDTH = 1080;
+//const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+TTF_Font * FONT = NULL;
 // }}}
 // prototypes {{{
 void sdlErr(std::string);// report an error related to SDL
 void imgErr(std::string);// report an error related to SDL_image
+void ttfErr(std::string);// report an error related to SDL_ttf
 void printPixelFmt(SDL_PixelFormat);// verbosely print all the data in a pixel format
 // }}}
 int main(int argc, char * argv[]) { // {{{
@@ -21,6 +26,16 @@ int main(int argc, char * argv[]) { // {{{
     //SDL_Surface * windowSurface = NULL;// a surface to be contained by the window
     SDL_Surface * chris = NULL;// test image
     SDL_Texture * chrisTex = NULL;
+    char chrisFile[] = "img/chris.png";// test file to use
+    //char chrisFile[] = "img/chris24.bmp";// test file to use
+    SDL_Rect chrisLocation;// where to draw the test image
+        chrisLocation.x = 0;
+        chrisLocation.y = 0;
+        chrisLocation.w = SCREEN_WIDTH;// if we want him to fill the screen
+        chrisLocation.h = SCREEN_HEIGHT;
+    float chrisScale = 4;// factor by which to increase the size of the chris "player"
+    double chrisAngle = 0;// deg, angle of rotation of chris
+    SDL_Point chrisCenter = {0,0};
     SDL_Surface * surfaceHelper = NULL;
     SDL_Surface * screenBG = NULL;// screen background
     SDL_Rect screenBG_rect;// a rectangle that fills the screen
@@ -31,14 +46,6 @@ int main(int argc, char * argv[]) { // {{{
     Uint8 bgAlpha = 0xFF;// opacity of the background
     int breatheDir = -1;// increment by which bgAlpha is changing
     //SDL_Texture * bgTex = NULL;
-    char chrisFile[] = "img/chris.png";// test file to use
-    //char chrisFile[] = "img/chris24.bmp";// test file to use
-    SDL_Rect chrisLocation;// where to draw the test image
-        chrisLocation.x = 0;
-        chrisLocation.y = 0;
-        chrisLocation.w = SCREEN_WIDTH;// if we want him to fill the screen
-        chrisLocation.h = SCREEN_HEIGHT;
-    float chrisScale = 4;// factor by which to increase the size of the chris "player"
     char boiFile[] = "img/datBoi_reel.png";
     SDL_Texture * boiTex = NULL;
     int boiFrames = 5;
@@ -54,6 +61,15 @@ int main(int argc, char * argv[]) { // {{{
     bool downHeld = false;
     SDL_Event event;// event handler
     SDL_ScaleMode scaleMode = SDL_ScaleModeLinear; // nearest, linear, or best (anisotropic)
+    SDL_Surface * textSurf = NULL;// surface to load text to
+    SDL_Texture * textTex = NULL;// texture to draw text with
+    SDL_Rect textRect = {100,300,100,100};
+    SDL_Color textColor = {0x00, 0x55, 0x00, 0xFF};// RGBA, color to render text
+    char textMessage[] = "Shut up, Parker.";// the text to be displayed
+    char fontPath[] = "TTF/IBMPlexSerif-Regular.ttf";
+    SDL_Point mousePos = {0,0};// position of the mouse
+    bool click = false;// whether the mouse is being clicked
+    int ptSize = 32;// pt, font size
     int delayTime = 50;// ms, delay between frames
     bool quit = false;// whether to quit the program
 
@@ -70,6 +86,10 @@ int main(int argc, char * argv[]) { // {{{
     std::cout << "initializing..." << std::endl;
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
         sdlErr("Failed to initialize SDL.");
+    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) < 0)// load the library for loading images
+        sdlErr("Failed to initialize image support.");
+    if (TTF_Init() < 0)// load the library for rendering text
+        ttfErr("Failed to initialize TTF.");
     if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) // nearest-pixel (0) may be preferrable for pixel art
         sdlErr("Linear texture filtering is not enabled.");
     window = SDL_CreateWindow( "SDL Learning", 
@@ -84,10 +104,9 @@ int main(int argc, char * argv[]) { // {{{
         sdlErr("The renderer could not be created.");
     if (SDL_SetRenderDrawColor(renderer, 0xFF, 0xAA, 0xAA, 0xFF))// color for Rect, Line, and Clear; RGBA
         sdlErr("Failed to set renderer draw color.");
-    if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) < 0)// load the libraries for loading images
-        sdlErr("Failed to initialize image support.");
     //windowSurface = SDL_GetWindowSurface(window);// get the reference to the window's surface (framebuffer)
     // }}}
+    // load resources {{{
     std::cout << "Loading texture directly from file..." << std::endl;
     chrisTex = IMG_LoadTexture(renderer, chrisFile);
     if (chrisTex == NULL)
@@ -105,6 +124,14 @@ int main(int argc, char * argv[]) { // {{{
     std::cout << "boiSrc.w: " << boiSrc.w << std::endl;
     boiDest.w = boiSrc.w;
     boiDest.h = boiSrc.h;
+    std::cout << "Loading font..." << std::endl;
+    FONT = TTF_OpenFont(fontPath, ptSize);
+    std::cout << "Rendering text to surface..." << std::endl;
+    textSurf = TTF_RenderText_Solid(FONT, textMessage, textColor);
+    textRect.w = textSurf->w;
+    textRect.h = textSurf->h;
+    std::cout << "Converting text surface to texture..." << std::endl;
+    textTex = SDL_CreateTextureFromSurface(renderer, textSurf);
     std::cout << "Setting texture scale mode: " << scaleMode << "..." << std::endl;
     SDL_SetTextureScaleMode(chrisTex, scaleMode);
     SDL_SetTextureScaleMode(boiTex, scaleMode);
@@ -137,6 +164,14 @@ int main(int argc, char * argv[]) { // {{{
                             std::cout << "right" << std::endl;
                             rightHeld = true;
                             break;
+                        case SDLK_q:
+                            std::cout << "q" << std::endl;
+                            chrisAngle--;
+                            break;
+                        case SDLK_e:
+                            std::cout << "e" << std::endl;
+                            chrisAngle++;
+                            break;
                         default:
                             std::cout << "unknown" << std:: endl;
                             break;
@@ -165,6 +200,12 @@ int main(int argc, char * argv[]) { // {{{
                             std::cout << "unknown" << std:: endl;
                             break;
                     }
+                case SDL_MOUSEBUTTONDOWN: // see also SDL_MOUSEMOTION, and friends
+                    click = true;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    click = false;
+                    break;
                 default:
                     //std::cout << "SDL event type: " << std::to_string(event.type) << std::endl;
                     break;
@@ -172,7 +213,7 @@ int main(int argc, char * argv[]) { // {{{
         }
         // }}}
 
-        // move chris
+        // move chris {{{
         chrisLocation.x = (chrisLocation.x + (rightHeld - leftHeld)*chrisSpeed )% SCREEN_WIDTH;
         chrisLocation.y = (chrisLocation.y + (downHeld - upHeld)*chrisSpeed )% SCREEN_HEIGHT;
         // wrap the chris pacman-style
@@ -182,6 +223,7 @@ int main(int argc, char * argv[]) { // {{{
             chrisLocation.y += SCREEN_HEIGHT;
         chrisLocation.x %= SCREEN_WIDTH;
         chrisLocation.y %= SCREEN_HEIGHT;
+        // }}}
 
         // draw stuff {{{ 
         SDL_SetRenderDrawColor(renderer, 0x33, 0x11, 0x11, 0xFF);// default color if nothing is drawn [to full opacity]
@@ -202,6 +244,12 @@ int main(int argc, char * argv[]) { // {{{
         for (i=0; i < 100; i++) {
             SDL_RenderDrawPoint(renderer, 3*i, 4*i);
         }
+        // draw a point where clicked
+        if (click) {
+            SDL_GetMouseState(&mousePos.x, &mousePos.y);
+            SDL_RenderDrawPoint(renderer, mousePos.x, mousePos.y);
+        }
+        // draw chris background
         if (bgAlpha >= 0xFF)
             breatheDir = -1;
         else if (bgAlpha <= 0)
@@ -216,7 +264,13 @@ int main(int argc, char * argv[]) { // {{{
         boiSrc.x = (boiSrc.x + boiSrc.w) % (boiSrc.w * boiFrames);
         // draw chris
         dstrect_dummy = chrisLocation;
-        SDL_RenderCopy(renderer, chrisTex, NULL, &dstrect_dummy);// render foreground texture (scales instead of crops)
+        //SDL_RenderCopy(renderer, chrisTex, NULL, &dstrect_dummy);// render foreground texture (scales instead of crops)
+        chrisCenter.x = chrisLocation.w/2;
+        chrisCenter.y = chrisLocation.h/2;
+        SDL_RenderCopyEx(renderer, chrisTex, NULL, &dstrect_dummy, chrisAngle, &chrisCenter, SDL_FLIP_NONE);
+        // draw text
+        dstrect_dummy = textRect;
+        SDL_RenderCopy(renderer, textTex, NULL, &dstrect_dummy);
         SDL_RenderPresent(renderer);// draw the rendering to the screen
         // }}}
 
@@ -239,8 +293,13 @@ int main(int argc, char * argv[]) { // {{{
     screenBG = NULL;
     SDL_DestroyTexture(chrisTex);
     chrisTex = NULL; // TODO: I'm not sure if it's necessary to set stuff to NULL like this.
+    TTF_CloseFont(FONT);
+    SDL_FreeSurface(textSurf);
+    SDL_DestroyTexture(textTex);
+    FONT = NULL;
     //SDL_DestroyTexture(bgTex);
     //bgTex = NULL;
+    TTF_Quit();
     IMG_Quit();
 	SDL_Quit();
     std::cout << "Goodbye." << std::endl;
@@ -255,4 +314,8 @@ void sdlErr(std::string message){
 void imgErr(std::string message){
     std::cerr << message << '\n'
               << "SDL_Image Error: " << IMG_GetError() << '\n';
+}
+void ttfErr(std::string message){
+    std::cerr << message << '\n'
+              << "SDL_ttf Error: " << TTF_GetError() << '\n';
 }
